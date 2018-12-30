f3a116ec7ba17559a67be00b210815b270bab66c
Aki Nishimura <akihiko4@g.ucla.edu>
2018-12-30 12:03:42 -0800

    Remove the test for the skew-normal.

--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -1,16 +1,10 @@
 import numpy as np
 
-from .distributions import BivariateGaussian, BivariateSkewNormal, BivariateBanana
+from .distributions import BivariateGaussian, BivariateBanana
 
 def test_bi_gaussian():
     x = np.ones(2)
     skewnorm = BivariateGaussian()
-    f = skewnorm.compute_logp_and_gradient
-    assert numerical_grad_is_close(f, x)
-
-def test_skew_normal():
-    x = np.ones(2)
-    skewnorm = BivariateSkewNormal()
     f = skewnorm.compute_logp_and_gradient
     assert numerical_grad_is_close(f, x)
 
