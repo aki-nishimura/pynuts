9ad387bd323e8f2ddb04cfc768895b4db6a5e6e2
Aki Nishimura <akihiko4@g.ucla.edu>
2019-01-03 20:04:11 -0800

    Rename the position variable in the test module.

--- tests/test_hmc.py
+++ tests/test_hmc.py
@@ -7,37 +7,37 @@ from .distributions import BivariateGaussian
 
 def test_trajectory_symmetry():
 
-    f, stability_limit, n_step, theta0, p0 = setup_gaussian_target()
+    f, stability_limit, n_step, q0, p0 = setup_gaussian_target()
     dt = .9 * stability_limit
 
-    logp0, grad0 = f(theta0)
-    theta, p, logp, info, info_reverse = simulate_forward_and_backward(
-        f, dt, n_step, theta0, p0, logp0, grad0, hamiltonian_tol=float('inf')
+    logp0, grad0 = f(q0)
+    q, p, logp, info, info_reverse = simulate_forward_and_backward(
+        f, dt, n_step, q0, p0, logp0, grad0, hamiltonian_tol=float('inf')
     )
     assert np.allclose(logp0, logp, atol=1e-10)
-    assert np.allclose(theta0, theta, atol=1e-10)
+    assert np.allclose(q0, q, atol=1e-10)
     assert np.allclose(p0, p, atol=1e-10)
 
 
 def test_early_termination_symmetry():
 
-    f, stability_limit, n_step, theta0, p0 = setup_gaussian_target()
+    f, stability_limit, n_step, q0, p0 = setup_gaussian_target()
     dt = .99 * stability_limit
     hamiltonian_fluctuation_range = 5.73633
 
     # Expect early termination.
     hamiltonian_tol = hamiltonian_fluctuation_range - .05
-    logp0, grad0 = f(theta0)
-    theta, p, logp, info, info_reverse = simulate_forward_and_backward(
-        f, dt, n_step, theta0, p0, logp0, grad0, hamiltonian_tol
+    logp0, grad0 = f(q0)
+    q, p, logp, info, info_reverse = simulate_forward_and_backward(
+        f, dt, n_step, q0, p0, logp0, grad0, hamiltonian_tol
     )
     assert info['instability_detected'] == info_reverse['instability_detected'] == True
 
     # Expect NO early termination.
     hamiltonian_tol = hamiltonian_fluctuation_range + .05
-    logp0, grad0 = f(theta0)
-    theta, p, logp, info, info_reverse = simulate_forward_and_backward(
-        f, dt, n_step, theta0, p0, logp0, grad0, hamiltonian_tol
+    logp0, grad0 = f(q0)
+    q, p, logp, info, info_reverse = simulate_forward_and_backward(
+        f, dt, n_step, q0, p0, logp0, grad0, hamiltonian_tol
     )
     assert info['instability_detected'] == info_reverse['instability_detected'] == False
 
@@ -48,23 +48,23 @@ def setup_gaussian_target():
     f = bi_gauss.compute_logp_and_gradient
     stability_limit = bi_gauss.get_stepsize_stability_limit()
     n_step = 100
-    theta0 = np.array([0., 0.])
+    q0 = np.array([0., 0.])
     p0 = np.array([1., 1.])
 
-    return f, stability_limit, n_step, theta0, p0
+    return f, stability_limit, n_step, q0, p0
 
 
-def simulate_forward_and_backward(f, dt, n_step, theta0, p0, logp0, grad0,
+def simulate_forward_and_backward(f, dt, n_step, q0, p0, logp0, grad0,
                                   hamiltonian_tol):
     # Forward dynamics.
-    theta, p, logp, grad, info = hmc.simulate_dynamics(
-        f, dt, n_step, theta0, p0, logp0, grad0, hamiltonian_tol
+    q, p, logp, grad, info = hmc.simulate_dynamics(
+        f, dt, n_step, q0, p0, logp0, grad0, hamiltonian_tol
     )
 
     # Reverse dynamics.
-    theta, p, logp, grad, reverse_info = hmc.simulate_dynamics(
-        f, dt, n_step, theta, -p, logp, grad, hamiltonian_tol
+    q, p, logp, grad, reverse_info = hmc.simulate_dynamics(
+        f, dt, n_step, q, -p, logp, grad, hamiltonian_tol
     )
     p = -p
 
-    return theta, p, logp, info, reverse_info
+    return q, p, logp, info, reverse_info
